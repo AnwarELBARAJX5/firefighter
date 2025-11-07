aba38b93268a6082413c99d07b3407ddca7bbf0e
AnwarELBARAJX5 <elbarrajanwar@gmail.com>
2025-11-07 11:53:18 +0100

    correction des imports dans l'interface

--- src/main/java/model/BoardContext.java
+++ src/main/java/model/BoardContext.java
@@ -1,7 +1,14 @@
+package model;
+
+import util.Position;
+import util.TargetStrategy;
+import java.util.*;
+
+
 public interface BoardContext {
 	public List<Position> update();
 	public int stepNumber();
 	public void extinguish(Position position);
-	public void  createFire(Position);
+	public void  createFire(Position position);
 
 }
