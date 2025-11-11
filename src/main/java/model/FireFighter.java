dab447afb25eaeb6549ba720ef19d932d9e20452
AnwarELBARAJX5 <elbarrajanwar@gmail.com>
2025-11-11 11:43:37 +0100

    création de la classe FireFighter.java

---
+++ src/main/java/model/FireFighter.java
@@ -0,0 +1,24 @@
+package model;
+import util.Position;
+import util.TargetStrategy;
+import java.util.*;
+
+
+public class FireFighter extends AbstractAgent{
+    private final TargetStrategy targetStrategy = new TargetStrategy();
+    public Firefighter(Position startPosition) {
+        super(startPosition);
+    }
+    @Override
+    public void update(BoardContext context) {
+        Position newFirefighterPosition = targetStrategy.neighborClosestToFire(this.position,context.getFirePositions(), context::getNeighbors);
+        this.position=newFirefighterPosition;
+        context.extinguish(this.position);
+        List<Position> neighbors=context.getNeighbors(this.position);
+        for(Position neighborsPos:neighbors){
+            if(context.getFirePositions().contains(neighborsPos)){
+                context.extinguish(neighborsPos);
+            }
+        }
+    }
+}
