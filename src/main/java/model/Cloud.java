9bd8eb92a92d8f97d3b4ab5cc6c55208512469d0
AnwarELBARAJX5 <elbarrajanwar@gmail.com>
2025-11-14 10:47:06 +0100

    Ajout de CLOUD pour les nuages

---
+++ src/main/java/model/Cloud.java
@@ -0,0 +1,13 @@
+package model;
+
+import util.Position;
+
+public class Cloud extends AbstractAgent{
+    public Cloud(Position startPosition){
+        super(startPosition);
+    }
+    @Override
+    public void update(BoardContext context) {
+
+    }
+}
