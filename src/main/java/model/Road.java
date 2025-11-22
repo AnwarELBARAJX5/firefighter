bf43a38c1ad2e5728b9eaa348cc5fcaacded3895
AnwarELBARAJX5 <elbarrajanwar@gmail.com>
2025-11-22 18:52:22 +0100

    impléementation de la classe ROAD

---
+++ src/main/java/model/Road.java
@@ -0,0 +1,28 @@
+package model;
+
+import util.Position;
+
+public class Road extends AbstractSurface{
+    public Road(Position position){
+        super(position);
+    }
+    @Override
+    public Position getPosition() {
+        return position;
+    }
+
+    @Override
+    public ModelElement getType() {
+        return ModelElement.ROAD;
+    }
+
+    @Override
+    public boolean isTraversable() {
+        return true;
+    }
+
+    @Override
+    public boolean isFlammable() {
+        return false;
+    }
+}
