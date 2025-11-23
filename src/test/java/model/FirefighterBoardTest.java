ec988ddcec2fbecdd34b64f2ab0d16ae0c3a8936
AnwarELBARAJX5 <elbarrajanwar@gmail.com>
2025-11-23 21:50:31 +0100

    correction des tests

--- src/test/java/model/FirefighterBoardTest.java
+++ src/test/java/model/FirefighterBoardTest.java
@@ -3,24 +3,38 @@ package model;
 import org.junit.jupiter.api.Test;
 import util.Position;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.*;
-
+import org.junit.jupiter.api.BeforeEach;
 public class FirefighterBoardTest {
+  private Map<ModelElement, Integer> config=new HashMap<>();
+  @BeforeEach
+  public void initializer(){
+    Map<ModelElement, Integer> config = new HashMap<>();
+    config.put(ModelElement.FIRE, 6);
+    config.put(ModelElement.FIREFIGHTER, 10);
+    config.put(ModelElement.CLOUD, 6);
+    config.put(ModelElement.MOUNTAIN, 10);
+    config.put(ModelElement.ROCK, 15);
+    config.put(ModelElement.MOTORIZEDFIREFIGHTER, 5);
+  }
+
   @Test
   void testColumnCount(){
-    Board<List<ModelElement>> board = new FirefighterBoard(20, 10, 1, 3,5);
+    Board<List<ModelElement>> board = new FirefighterBoard(20, 10, config);
     assertThat(board.columnCount()).isEqualTo(20);
   }
   @Test
   void testRowCount(){
-    Board<List<ModelElement>> board = new FirefighterBoard(20, 10, 1, 3,5);
+    Board<List<ModelElement>> board = new FirefighterBoard(20, 10, config);
     assertThat(board.rowCount()).isEqualTo(10);
   }
   @Test
   void testStepNumber(){
-    Board<List<ModelElement>> board = new FirefighterBoard(20, 10, 1, 3,5);
+    Board<List<ModelElement>> board = new FirefighterBoard(20, 10, config);
     for(int index = 0; index < 10; index++){
       assertThat(board.stepNumber()).isEqualTo(index);
       board.updateToNextGeneration();
@@ -29,7 +43,7 @@ public class FirefighterBoardTest {
   }
   @Test
   void testGetState_afterSet(){
-    Board<List<ModelElement>> board = new FirefighterBoard(20, 10, 0, 0,5);
+    Board<List<ModelElement>> board = new FirefighterBoard(20, 10, config);
     Position position = new Position(1,2);
     assertThat(board.getState(position)).isEmpty();
     board.setState(List.of(ModelElement.FIRE), position);
