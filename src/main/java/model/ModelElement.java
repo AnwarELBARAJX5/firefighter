d0aac76672c0f6bb5cd3e6b7abd054ef01b4ce28
AnwarELBARAJX5 <elbarrajanwar@gmail.com>
2025-11-25 21:55:10 +0100

    début tache 3: implémentation du modéle virus population médecin

--- src/main/java/model/ModelElement.java
+++ src/main/java/model/ModelElement.java
@@ -4,7 +4,9 @@ import view.ViewElement;
 
 public enum ModelElement {
 
-  FIREFIGHTER(ViewElement.FIREFIGHTER,10), FIRE(ViewElement.FIRE,5) ,CLOUD(ViewElement.CLOUD,8),MOTORIZEDFIREFIGHTER(ViewElement.MOTORIZEDFIREFIGHTER,10),MOUNTAIN(ViewElement.MOUNTAIN,1),ROAD(ViewElement.ROAD,0),ROCK(ViewElement.ROCK,2);
+  FIREFIGHTER(ViewElement.FIREFIGHTER,10), FIRE(ViewElement.FIRE,5) ,CLOUD(ViewElement.CLOUD,8),MOTORIZEDFIREFIGHTER(ViewElement.MOTORIZEDFIREFIGHTER,10),MOUNTAIN(ViewElement.MOUNTAIN,1),ROAD(ViewElement.ROAD,0),ROCK(ViewElement.ROCK,2),DOCTOR(ViewElement.DOCTOR, 10),
+  VIRUS(ViewElement.VIRUS, 5),
+  PERSON(ViewElement.PERSON, 8);
   public ViewElement view;
   private final int priority;
   ModelElement(ViewElement view,int priority){
--- src/main/java/view/ViewElement.java
+++ src/main/java/view/ViewElement.java
@@ -4,7 +4,9 @@ import javafx.scene.paint.Color;
 import model.Cloud;
 
 public enum ViewElement {
-  FIREFIGHTER(Color.BLUE), FIRE(Color.RED), EMPTY(Color.WHITE),CLOUD(Color.CYAN),MOTORIZEDFIREFIGHTER(Color.CADETBLUE),MOUNTAIN(Color.BROWN),ROAD(Color.GRAY),ROCK(Color.LIGHTSALMON);
+  FIREFIGHTER(Color.BLUE), FIRE(Color.RED), EMPTY(Color.WHITE),CLOUD(Color.CYAN),MOTORIZEDFIREFIGHTER(Color.CADETBLUE),MOUNTAIN(Color.BROWN),ROAD(Color.GRAY),ROCK(Color.LIGHTSALMON),VIRUS(Color.LIMEGREEN), // Vert maladif
+          PERSON(Color.YELLOW),   // Jaune (neutre)
+          DOCTOR(Color.WHITE);
   final Color color;
   ViewElement(Color color) {
     this.color = color;
