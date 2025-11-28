0f049402480c7c8e9df76f01196dffb345f30af1
AnwarELBARAJX5 <elbarrajanwar@gmail.com>
2025-11-28 08:13:41 +0100

    ressource code de HexagonGrid + Diagramme

--- src/main/java/view/ViewElement.java
+++ src/main/java/view/ViewElement.java
@@ -5,7 +5,7 @@ import javafx.scene.paint.Color;
 
 public enum ViewElement {
   FIREFIGHTER(Color.BLUE), FIRE(Color.RED), EMPTY(Color.WHITE),CLOUD(Color.CYAN),MOTORIZEDFIREFIGHTER(Color.CADETBLUE),MOUNTAIN(Color.BROWN),ROAD(Color.GRAY),ROCK(Color.LIGHTSALMON),VIRUS(Color.LIMEGREEN), // Vert maladif
-          PERSON(Color.YELLOW),   // Jaune (neutre)
+          PERSON(Color.YELLOW),
           DOCTOR(Color.GREY);
   final Color color;
   ViewElement(Color color) {
